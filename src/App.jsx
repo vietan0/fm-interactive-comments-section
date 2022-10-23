a9318e69cc7a0b75b0324212865862f7b9dd0edc
Việt An <vietan00892b@gmail.com>
2022-10-23 11:00:06 +0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div className="App">
+      <div className="attribution">
+        Challenge by
+        {' '}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by
+      </div>
+    </div>
+  );
+}
+
+export default App;
